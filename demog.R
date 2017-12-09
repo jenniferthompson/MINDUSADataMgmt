50bfdca68d6f9775ff10b82513f4b43439fa5360
Jennifer Thompson <thompson.jennifer@gmail.com>
2017-12-08 18:57:02 -0600

    Calculate APACHE, SOFA at baseline

--- demog.R
+++ demog.R
@@ -161,10 +161,16 @@ baseline <- demog_raw %>%
       labels = names(get_levels_ih("frailty"))
     ),
     ## Also want numeric version of frailty
-    frailty = frailty + 1
-  ) %>%
+    frailty = frailty + 1,
+    uo_enr_f = factor(
+      ifelse(!is.na(uo_enr) & uo_enr > 0, uo_enr, NA),
+      levels = 1:3,
+      labels = c("0-200", "201-500", "500+")
+    )
+  )
   
-  ## Create new variables
+## -- Straightforward new variables --------------------------------------------
+baseline <- baseline %>%
   mutate(
     age_consent = as.numeric(difftime(enroll_date, dob, units = "days")) / 365.25,
     ## Race: combine categories using NIH docs at
@@ -200,7 +206,7 @@ baseline <- demog_raw %>%
       ifelse(home_meds_antipsych_0 & num_home_antipsyc == 1, 1,
       ifelse(num_home_antipsyc > 0, 2, NA)),
       levels = 1:2, labels = c("None", "At least one")
-    ),
+    )
     ## Primary admission diagnosis (TBD pending PI input); one suggestion:
     # Sepsis, septic shock, ARDS d/t infection (1, 2)
     # ARDS without infection, airway issues (3, 4)
@@ -215,8 +221,12 @@ baseline <- demog_raw %>%
     # Non-transplant surgery (19, 20, 22 - 27)
     # Transplant (21)
     # Any other (99)
-    
-    ## -- Charlson score -------------------------------------------------------
+  )
+
+## -- More complicated variables -----------------------------------------------
+baseline <- baseline %>%
+  ## -- Charlson score ---------------------------------------------------------
+  mutate(
     ## 1. How many actual conditions in each category did patient have?
     ##    (Does not include "None")
     charlson_cond_1 =
@@ -259,3 +269,292 @@ baseline <- demog_raw %>%
     ## 3. Total Charlson = sum of all point levels
     charlson_total = charlson_1 + charlson_2 + charlson_3 + charlson_6
   )
+
+## -- APACHE II score --------------------------------------------------------
+## Reference: Knaus et al, CCM 1985 Oct; 13(10):818-29
+## https://www.ncbi.nlm.nih.gov/pubmed/3928249
+baseline <- baseline %>%
+  mutate(
+    ## Temperature
+    temp_ap = case_when(
+      is.na(temp_enr_high)  | is.na(temp_enr_low) ~ as.numeric(NA),
+      temp_enr_high >= 41   | temp_enr_low < 30   ~ 4,
+      temp_enr_high >= 39   | temp_enr_low < 32   ~ 3,
+                              temp_enr_low < 34   ~ 2,
+      temp_enr_high >= 38.5 | temp_enr_low < 36   ~ 1,
+      TRUE                                        ~ 0
+    ),
+    
+    ## Mean arterial pressure
+    map_ap = case_when(
+      is.na(map_enr_high) | is.na(map_enr_low) ~ as.numeric(NA),
+      map_enr_high >= 160 | map_enr_low <= 49  ~ 4,
+      map_enr_high >= 130                      ~ 3,
+      map_enr_high >= 110 | map_enr_low <= 70  ~ 2,
+      TRUE                                     ~ 0
+    ),
+    
+    ## Heart rate
+    hr_ap = case_when(
+      is.na(hr_enr_high) | is.na(hr_enr_low) ~ as.numeric(NA),
+      hr_enr_high >= 180 | hr_enr_low <= 39  ~ 4,
+      hr_enr_high >= 140 | hr_enr_low <= 54  ~ 3,
+      hr_enr_high >= 110 | hr_enr_low <= 69  ~ 2,
+      TRUE ~ 0
+    ),
+    
+    ## Respiratory rate
+    rr_ap = case_when(
+      is.na(rr_enr_high) | is.na(rr_enr_low) ~ as.numeric(NA),
+      rr_enr_high >= 50  | rr_enr_low <= 5   ~ 4,
+      rr_enr_high >= 35                      ~ 3,
+                           rr_enr_low <= 9   ~ 2,
+      rr_enr_high >= 25  | rr_enr_low <= 11  ~ 1,
+      TRUE                                   ~ 0
+    ),
+    
+    ## Oxygenation: the fun part!
+    ## If oxygenation info available (FiO2, PCO2, PaO2), calculate A-a gradient:
+    ## ((713 * FiO2) - (PCO2 / 0.8)) - PaO2; formula confirmed by EWE Dec 2017
+    aa_ap = ifelse(
+      is.na(fio2_enr) | is.na(pco2_enr) | is.na(pao2_enr), NA,
+      ((713 * fio2_enr) - (pco2_enr / 0.8)) - pao2_enr
+    ),
+    
+    ## If FiO2 >= 0.5, assign points using A-a gradient;
+    ##   if < 0.5, assign points using PaO2
+    oxy_ap = case_when(
+      is.na(fio2_enr)                  ~ as.numeric(NA),
+      fio2_enr >= 0.5 & aa_ap >= 500   ~ 4,
+      fio2_enr >= 0.5 & aa_ap >= 350   ~ 3,
+      fio2_enr >= 0.5 & aa_ap >= 200   ~ 2,
+      fio2_enr >= 0.5 & aa_ap < 200    ~ 0,
+      fio2_enr < 0.5  & pao2_enr < 55  ~ 4,
+      fio2_enr < 0.5  & pao2_enr <= 60 ~ 3,
+      fio2_enr < 0.5  & pao2_enr <= 70 ~ 1,
+      TRUE                             ~ 0
+    ),
+    
+    ## Acid (pH, or alternatively CO2)
+    acid_ap = case_when(
+      ## If pH is available, use that
+      !is.na(ph_enr) & (ph_enr >= 7.7 | ph_enr < 7.15) ~ 4,
+      !is.na(ph_enr) & (ph_enr >= 7.6 | ph_enr < 7.25) ~ 3,
+      !is.na(ph_enr) &                  ph_enr < 7.33  ~ 2,
+      !is.na(ph_enr) &  ph_enr >= 7.5                  ~ 1,
+      !is.na(ph_enr)                                   ~ 0,
+      ## If neither pH nor CO2 (HCO3) is available, score is missing
+      is.na(co2_enr_high) | is.na(co2_enr_low) ~ as.numeric(NA),
+      ## If CO2 (HCO3) is available, use that
+      co2_enr_high >= 52 | co2_enr_low < 15 ~ 4,
+      co2_enr_high >= 41 | co2_enr_low < 18 ~ 3,
+                           co2_enr_low < 22 ~ 2,
+      co2_enr_high >= 32                    ~ 1,
+      TRUE                                  ~ 0
+    ),
+    
+    ## Sodium
+    na_ap = case_when(
+      is.na(na_enr_high) | is.na(na_enr_low) ~ as.numeric(NA),
+      na_enr_high >= 180 | na_enr_low <= 110 ~ 4,
+      na_enr_high >= 160 | na_enr_low <= 119 ~ 3,
+      na_enr_high >= 155 | na_enr_low <= 129 ~ 2,
+      na_enr_high >= 150                     ~ 1,
+      TRUE                                   ~ 0
+    ),
+    
+    ## Potassium
+    k_ap = case_when(
+      is.na(k_enr_high) | is.na(k_enr_low) ~ as.numeric(NA),
+      k_enr_high >= 7   | k_enr_low < 2.5  ~ 4,
+      k_enr_high >= 6                      ~ 3,
+                          k_enr_low < 3    ~ 2,
+      k_enr_high >= 5.5 | k_enr_low < 3.5  ~ 1,
+      TRUE                                 ~ 0
+    ),
+    
+    ## Creatinine
+    cr_ap = case_when(
+      is.na(cr_enr_high) | is.na(cr_enr_low) ~ as.numeric(NA),
+      cr_enr_high >= 3.5                     ~ 4,
+      cr_enr_high >= 2                       ~ 3,
+      cr_enr_high >= 1.5 | cr_enr_low < 0.6  ~ 2,
+      TRUE                                   ~ 0
+    ),
+    
+    ## Hematocrit/PCV
+    pcv_ap = case_when(
+      is.na(pcv_enr_high) | is.na(pcv_enr_low) ~ as.numeric(NA),
+      pcv_enr_high >= 60 | pcv_enr_low < 20    ~ 4,
+      pcv_enr_high >= 50 | pcv_enr_low < 30    ~ 2,
+      pcv_enr_high >= 46                       ~ 1,
+      TRUE                                     ~ 0
+    ),
+    
+    ## White blood count
+    wbc_ap = case_when(
+      is.na(wbc_enr_high) | is.na(wbc_enr_low) ~ as.numeric(NA),
+      wbc_enr_high >= 40 | wbc_enr_low < 1     ~ 4,
+      wbc_enr_high >= 20 | wbc_enr_low < 3     ~ 2,
+      wbc_enr_high >= 15                       ~ 1,
+      TRUE                                     ~ 0
+    ),
+    
+    ## Glasgow Coma Score
+    gcs_ap = ifelse(is.na(gcs_enr) | gcs_enr == 99, NA, 15 - gcs_enr),
+    
+    ## Age
+    age_ap = case_when(
+      is.na(age_consent) ~ as.numeric(NA),
+      age_consent < 45   ~ 0,
+      age_consent < 55   ~ 2,
+      age_consent < 65   ~ 3,
+      age_consent < 75   ~ 5,
+      TRUE               ~ 6
+    ),
+    
+    ## Chronic disease
+    ## 1. How many actual conditions did patient have?
+    chronic_conditions =
+      rowSums(.[, str_subset(names(.), "^chronic\\_dis\\_[1-5]$")]),
+    ## 2. If patient had >1 condition *and* "none" marked, *or* neither "none"
+    ##    nor any conditions marked, # conditions = NA
+    chronic_conditions = ifelse(
+      (chronic_conditions > 0 & chronic_dis_0) |
+        (chronic_conditions == 0 & chronic_dis_0 == 0), NA,
+      chronic_conditions
+    ),
+    ## 3. If patient had no chronic conditions, 0 points; otherwise, if they
+    ##    had elective surgery, 2 points; otherwise, 5 points
+    chrondis_ap = case_when(
+      is.na(chronic_conditions) |
+        (chronic_conditions > 0 & is.na(apache_chronic_points)) ~ as.numeric(NA),
+      chronic_conditions == 0    ~ 0,
+      apache_chronic_points == 2 ~ 2,
+      TRUE                       ~ 5
+    )
+  )
+
+## Sum components to get versions of APACHE II
+## Vector of APS component names
+vars_apache_aps <- paste0(
+  c("temp", "map", "hr", "rr", "oxy", "acid", "na", "k", "cr", "pcv",
+    "wbc", "gcs"),
+  "_ap"
+)
+## Vector of overall APACHE component names
+vars_apache_all <- c(vars_apache_aps, "age_ap", "chrondis_ap")
+
+## How many components are available?
+baseline$apache_vars_avail <- rowSums(!is.na(baseline[, vars_apache_all]))
+baseline$apache_total_miss0 <- ifelse(
+  baseline$apache_vars_avail == 0, NA,
+  rowSums(baseline[, vars_apache_all], na.rm = TRUE)
+)
+baseline$apache_total_missNA <- ifelse(
+  baseline$apache_vars_avail == 0, NA,
+  rowSums(baseline[, vars_apache_all], na.rm = FALSE)
+)
+
+# ## Plot two different versions
+# ggplot(data = baseline) +
+#   geom_histogram(aes(x = apache_total_miss0), fill = "navy", binwidth = 1, alpha = 0.4) +
+#   geom_histogram(aes(x = apache_total_missNA), fill = "darkgreen", binwidth = 1, alpha = 0.4)
+
+## -- SOFA score ---------------------------------------------------------------
+## References:
+##   Original SOFA paper, Vincent et al (https://doi.org/10.1007/BF01709751)
+##   S/F as substitute for unavailable P/F:
+##     Pandharipande et al https://www.ncbi.nlm.nih.gov/pubmed/19242333
+baseline <- baseline %>%
+  mutate(
+    ## Respiratory component: Use P/F if available; otherwise use S/F
+    resp_sofa = case_when(
+      is.na(pfratio_worst_enr) & is.na(sf_enr) ~ as.numeric(NA),
+      pfratio_worst_enr <= 100  ~ 4,
+      pfratio_worst_enr <= 200  ~ 3,
+      pfratio_worst_enr <= 300  ~ 2,
+      pfratio_worst_enr <= 400  ~ 1,
+      !is.na(pfratio_worst_enr) ~ 0,
+      sf_enr <= 89              ~ 4,
+      sf_enr <= 214             ~ 3,
+      sf_enr <= 357             ~ 2,
+      sf_enr <= 512             ~ 1,
+      TRUE                      ~ 0
+    ),
+    
+    ## Coagulation
+    coag_sofa = case_when(
+      is.na(plt_enr) ~ as.numeric(NA),
+      plt_enr <= 20  ~ 4,
+      plt_enr <= 50  ~ 3,
+      plt_enr <= 100 ~ 2,
+      plt_enr <= 150 ~ 1,
+      TRUE           ~ 0
+    ),
+    
+    ## Liver
+    liver_sofa = case_when(
+      is.na(bilirubin_enr) ~ 0,
+        ## assume no bilirubin = no clinical indication of liver dysfunction
+        ## confirmed by PIs on...
+      bilirubin_enr >= 12  ~ 4,
+      bilirubin_enr >= 6   ~ 3,
+      bilirubin_enr >= 2   ~ 2,
+      bilirubin_enr >= 1.2 ~ 1,
+      TRUE                 ~ 0
+    ),
+    
+    ## Central nervous system
+    cns_sofa = case_when(
+      is.na(gcs_enr) | gcs_enr == 99 ~ as.numeric(NA),
+      gcs_enr < 6  ~ 4,
+      gcs_enr < 10 ~ 3,
+      gcs_enr < 13 ~ 2,
+      gcs_enr < 15 ~ 1,
+      TRUE         ~ 0
+    ),
+    
+    ## Renal: Look at highest creatinine *and* urine output
+    renal_sofa = case_when(
+      is.na(cr_enr_high) | is.na(uo_enr_f)       ~ as.numeric(NA),
+      cr_enr_high >= 5   | uo_enr_f == "0-200"   ~ 4,
+      cr_enr_high >= 3.5 | uo_enr_f == "201-500" ~ 3,
+      cr_enr_high >= 2                           ~ 2,
+      cr_enr_high >= 1.2                         ~ 1,
+      TRUE                                       ~ 0
+    )
+  ) %>%
+  ## Cardiovascular: already calculated; rename for consistency, create factor
+  ##  version for Table 1
+  rename(cv_sofa = "sofa_cv_enr") %>%
+  mutate(
+    cv_sofa_f = factor(
+      cv_sofa,
+      levels = get_levels_ih("sofa_cv_enr"),
+      labels = get_levels_ih("sofa_cv_enr")
+    )
+  )
+
+## Sum components to get versions of SOFA
+## Vector of modified SOFA component names (leaves out CNS; GCS tricky w/ MV pts)
+vars_sofa_mod <- paste0(c("resp", "coag", "liver", "renal", "cv"), "_sofa")
+## Vector of overall SOFA component names
+vars_sofa_all <- c(vars_sofa_mod, "cns_sofa")
+
+## How many components are available?
+baseline$sofa_vars_avail <- rowSums(!is.na(baseline[, vars_sofa_all]))
+baseline$sofa_total_miss0 <- ifelse(
+  baseline$sofa_vars_avail == 0, NA,
+  rowSums(baseline[, vars_sofa_all], na.rm = TRUE)
+)
+baseline$sofa_total_missNA <- ifelse(
+  baseline$sofa_vars_avail == 0, NA,
+  rowSums(baseline[, vars_sofa_all], na.rm = FALSE)
+)
+
+# ## Plot two different versions
+# ggplot(data = baseline) +
+#   geom_histogram(aes(x = sofa_total_miss0), fill = "navy", binwidth = 1, alpha = 0.4) +
+#   geom_histogram(aes(x = sofa_total_missNA), fill = "darkgreen", binwidth = 1, alpha = 0.4)
+
