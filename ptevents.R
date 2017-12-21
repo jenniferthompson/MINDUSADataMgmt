70ec9942e2a9b59da1169e015d72629ea35f55d2
Jennifer Thompson <thompson.jennifer@gmail.com>
2017-12-21 13:48:56 -0600

    Now creates two datasets: one with every REDCap event for all patients, and one with one record per intervention + post-IT day for all randomized patients (even after discharge/death/withdrawal)

--- ptevents.R
+++ ptevents.R
@@ -24,8 +24,7 @@ rand_pts <- ptstatus_df %>%
   filter(randomized & !excluded_ever) %>%
   pull(id)
 
-## -- Import REDCap event names, whether patient in ICU (from ABCDEF form), ----
-## -- enrollment and randomization times if applicable -------------------------
+## -- Import REDCap event names, whether patient in ICU (from ABCDEF form) -----
 events_raw <- import_df(
   rctoken = "MINDUSA_IH_TOKEN",
   id_field = "id",
@@ -55,7 +54,7 @@ last_pr_day <- events_raw %>%
 
 ## Add randomization event onto daily data; calculate days to each event
 ## 0 = day of consent/randomization
-events_df <- events_raw %>%
+allpts_events <- events_raw %>%
   left_join(last_pr_day %>% select(-event_num), by = "id") %>%
   group_by(id) %>%
   mutate(
@@ -67,6 +66,91 @@ events_df <- events_raw %>%
     id, redcap_event_name, days_since_consent, days_since_randomization, in_icu
   )
 
-## Save to analysisdata
-saveRDS(events_df, file = "analysisdata/rds/ptevents.rds")
-write_csv(events_df, path = "analysisdata/csv/ptevents.csv")
+## -- Create dummy data set for all *randomized* patients, with status on ------
+## -- each day up to IT day 18 (last possible event in database) ---------------
+
+## Download necessary dates/times from dates tracking form
+time_vars <- c("enroll_time", "randomization_time", "death_time", "hospdis_time")
+
+dates_raw <- import_df(
+  rctoken = "MINDUSA_IH_TOKEN",
+  id_field = "id",
+  export_labels = "none",
+  fields = c("id", "studywd_time", time_vars),
+  events = "enrollment__day_0_arm_1"
+) %>%
+  ## Only want randomized patients
+  filter(id %in% rand_pts) %>%
+  ## Make date/time variables
+  mutate_at(time_vars, ymd_hm) %>%
+  mutate_at(time_vars, funs("date" = date)) %>%
+  mutate(studywd_date = as.Date(studywd_time, format = "%Y-%m-%d")) %>%
+  rename_at(paste0(time_vars, "_date"), ~ str_replace(., "\\_time", ""))
+
+## Create data frame with, for each randomized patient, one record per
+## patient-day, day of randomization -> IT day 18 (final event in database)
+randpts_events <- cross_df(
+  list(
+    id = rand_pts,
+    redcap_event_name = setdiff(
+      ih_events$unique_event_name,
+      c("randomization_arm_1", "prior_to_hospital_arm_1")
+    )
+  )
+) %>%
+  ## Keep only day of randomization + anything in interventional period
+  ## Patients could be randomized on any of five REDCap events, so this is a
+  ##  little tricky
+  left_join(select(ih_events, unique_event_name, event_num),
+            by = c("redcap_event_name" = "unique_event_name")) %>%
+  left_join(last_pr_day %>% select(id, randomization_event, randomization_day),
+            by = "id") %>%
+  filter(redcap_event_name == randomization_event |
+           str_detect(redcap_event_name, "^interventional")) %>%
+  select(id, redcap_event_name) %>%
+  
+  ## Add info from dates tracking, days since consent/randomization, whether
+  ##   patient in ICU
+  left_join(dates_raw %>% select(-redcap_event_name), by = "id") %>%
+  left_join(allpts_events, by = c("id", "redcap_event_name")) %>%
+  
+  ## Determine *date* for each record, patient's overall status in the study on
+  ##  each event, other indicators of patient status
+  group_by(id) %>%
+  mutate(
+    study_day = 1:n() - 1,
+    date_today = randomization_date + study_day,
+    in_redcap = !is.na(days_since_randomization),
+    intervention = study_day %in% 0:13,
+    postintervention = study_day > 13,
+    study_status = factor(
+      case_when(
+        intervention & in_redcap ~ 1,
+        postintervention & in_redcap ~ 2,
+        !is.na(hospdis_date) & date_today >= hospdis_date ~ 4,
+        !is.na(death_date) & date_today >= death_date ~ 5,
+        !is.na(studywd_date) & date_today >= studywd_date ~ 6,
+        TRUE ~ 3
+      ),
+      levels = 1:6,
+      labels = c("Intervention", "Post-intervention",
+                 "Hospitalized; no longer being assessed",
+                 "Discharged", "Deceased", "Withdrawn")
+    ),
+    ## Replace missing days since consent (after patient left hospital/followup);
+    ##  "study_day" now equivalent to days since randomization
+    days_since_consent = days_diff(date_today, enroll_date),
+    ## Indicator for "still in hospital": has record in REDCap, or has not yet
+    ## died/been discharged/withdrawn
+    hospitalized =
+      in_redcap | study_status == "Hospitalized; no longer being assessed"
+  ) %>%
+  ## Remove dates, select only variables we need
+  select(id, redcap_event_name, study_day, days_since_consent, in_redcap,
+         hospitalized, intervention, postintervention, in_icu, study_status)
+
+## -- Save both final dataframes to analysisdata -------------------------------
+saveRDS(allpts_events, file = "analysisdata/rds/allptevents.rds")
+saveRDS(randpts_events, file = "analysisdata/rds/randptevents.rds")
+write_csv(allpts_events, path = "analysisdata/csv/allptevents.csv")
+write_csv(randpts_events, path = "analysisdata/csv/randptevents.csv")
