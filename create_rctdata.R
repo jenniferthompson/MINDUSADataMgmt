10269e805a5a4c24d4bd1ea6f101602f9ad64a08
Jennifer Thompson <thompson.jennifer@gmail.com>
2017-12-22 16:50:25 -0600

    Add outputs from pad.R. Update outputs from ptevents.R.

--- create_rctdata.R
+++ create_rctdata.R
@@ -8,11 +8,15 @@ source("fake_make.R")
 
 ## -- Read in datasets from /analysisdata --------------------------------------
 ptstatus_df <- readRDS("analysisdata/rds/ptstatus.rds")
-ptevents_df <- readRDS("analysisdata/rds/ptevents.rds")
+allptevents_df <- readRDS("analysisdata/rds/allptevents.rds")
+randptevents_df <- readRDS("analysisdata/rds/randptevents.rds")
 admission_df <- readRDS("analysisdata/rds/admission.rds")
 datestrack_df <- readRDS("analysisdata/rds/datestrack.rds")
 ptdrug_df <- readRDS("analysisdata/rds/ptdrug.rds")
 doses_df <- readRDS("analysisdata/rds/doses.rds")
+padasmts_df <- readRDS("analysisdata/rds/padasmts.rds")
+paddaily_df <- readRDS("analysisdata/rds/paddaily.rds")
+padsummary_df <- readRDS("analysisdata/rds/padsummary.rds")
 
 ## -- Temporary: Create dataset of fake treatment groups -----------------------
 ## Final treatment groups will be added once data clean is finalized, database
@@ -31,6 +35,6 @@ trt_df <- data.frame(
 )
 
 ## -- Save datasets to final RCT .Rdata file -----------------------------------
-save(trt_df, ptstatus_df, ptevents_df, admission_df, datestrack_df, ptdrug_df,
-     doses_df,
+save(trt_df, ptstatus_df, allptevents_df, randptevents_df, admission_df,
+     datestrack_df, ptdrug_df, doses_df, padasmts_df, paddaily_df, padsummary_df,
      file = "../MINDUSARCT/analysisdata/rct.Rdata")
