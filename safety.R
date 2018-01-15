5ba966d6a3b66d37b0e5f6208ebb5338db604e25
Jennifer Thompson <thompson.jennifer@gmail.com>
2018-01-15 17:55:56 -0600

    Start work on safety form/outcomes

---
+++ safety.R
@@ -0,0 +1,132 @@
+################################################################################
+## Data management for Daily Safety Assessment form
+## This form is filled out once daily through Intervention Day 14, and possibly
+##   through Intervention Day 18 if needed for four days post-study drug
+################################################################################
+
+library(tidyverse)
+library(stringr)
+library(lubridate)
+library(assertr)
+
+## Source data management functions
+source("data_functions.R")
+
+## Read in datasets with all study events for randomized, consented patients;
+## will need them to calculate X within various time periods
+allpts_events <- readRDS("analysisdata/rds/allptevents.rds")
+randpts_events <- readRDS("analysisdata/rds/randptevents.rds")
+
+## -- Import data dictionaries from REDCap -------------------------------------
+## All tokens are stored in .Renviron
+ih_dd <- get_datadict("MINDUSA_IH_TOKEN")
+ih_events <- get_events("MINDUSA_IH_TOKEN") %>% mutate(event_num = 1:nrow(.))
+## Add event_num to help with sorting events later
+ih_mapping <- get_event_mapping("MINDUSA_IH_TOKEN")
+
+## -- Bring in ptstatus_df; we'll use some variables from there ----------------
+ptstatus_df <- readRDS("analysisdata/rds/ptstatus.rds")
+
+## Vector of all randomized patients never excluded
+rand_pts <- ptstatus_df %>%
+  filter(randomized & !excluded_ever) %>%
+  pull(id)
+
+## -- Download daily safety form, plus a few other variables for Torsades ------
+## Torsades can be recorded in several places:
+## - Daily Data Collection form, as a type of arrhythmia
+## - 12-Lead ECG Report form, as an ECG interpretation
+## - Study Drug Admin form, as a reason study drug was permanently discontinued
+## - Event Reporting form, as a type of AE
+## Options 1-3 are downloaded alongside additional safety data; they are
+## recorded on a daily basis. The AE form is on the Enrollment event and thus
+## AEs are downloaded separately.
+safety_raw <- import_df(
+  rctoken = "MINDUSA_IH_TOKEN",
+  id_field = "id",
+  export_labels = "none",
+  fields = c("id", "redcap_event_name", "arrhythmia_daily",
+             str_subset(ih_dd$field_name, "ecg\\_interpretation"),
+             str_subset(ih_dd$field_name, "permanent\\_stop\\_why")),
+  forms = c("daily_safety_assessment_form"),
+  events = setdiff(
+    ih_events$unique_event_name,
+    c("randomization_arm_1", "prior_to_hospital_arm_1")
+  )
+) %>%
+  ## Remove unneeded variables
+  select(
+    -daily_safety_assessment_form_complete,
+    ## ECG interpretation and arrhythmias are checkboxes; for Torsades, we're
+    ## only interested in one option each (arrhythmia #4, ECG #5)
+    -matches("^arrhythmia\\_daily\\_[1-3, 5, 9]+$"),
+    -matches("^ecg\\_interpretation\\_[1-5]\\_[0-4, 6]$")
+  ) %>%
+  ## Restrict to "real" events, as determined in ptevents.R
+  right_join(allpts_events, by = c("id", "redcap_event_name"))
+
+ae_raw <- import_df(
+  rctoken = "MINDUSA_IH_TOKEN",
+  id_field = "id",
+  export_labels = "none",
+  ## Also export date/times of enrollment and randomization, to determine what
+  ## study day the event occurred on
+  fields = c("id", "enroll_time", "randomization_time"),
+  forms = c("event_reporting_form"),
+  events = "enrollment__day_0_arm_1"
+) %>%
+  mutate_at(vars("enroll_time", "randomization_time"), ymd_hm) %>%
+  mutate_at(vars(starts_with("ae_date")), ymd)
+
+## -- Extrapyramidal symptoms (EPS) assessment ---------------------------------
+## List of variables involved in EPS scoring
+eps_vars <- c("elbow_rigidity", "wrist_rigidity", "glabella_tap",
+              "resting_tremor", "salivation")
+
+eps_df <- safety_raw %>%
+  dplyr::select(id, redcap_event_name, one_of(eps_vars)) %>%
+  ## All EPS variables have numeric values which reflect actual score, except
+  ## that 5 = "not done"; in addition, glabellar tap has a value of 99 = UTA
+  ## Assign 5, 99 to NA
+  mutate_at(vars(one_of(eps_vars)), funs(ifelse(. %in% c(5, 99), NA, .))) %>%
+  ## How many EPS symptoms are missing each day?
+  mutate(eps_symptoms_avail = rowSums(!is.na(.[, eps_vars]))) %>%
+  ## If all symptoms are present, EPS score = sum of all components / 5
+  ## For now, if >=1 symptom is missing, total score is missing
+  mutate(
+    eps_total = ifelse(eps_symptoms_avail < 5, NA, rowMeans(.[, eps_vars]))
+  )
+
+eps_ge3 <- eps_df %>%
+  summarise_at(vars(one_of(eps_vars)), funs(sum(. >= 3, na.rm = TRUE)))
+
+## -- Torsades de pointes ------------------------------------------------------
+## Eventual goal: Have an indicator for whether patient experienced Torsades on
+## a given day, and an indicator for whether patient ever experienced Torsades
+## at all.
+
+## Adverse event data
+## Reshape to one row per event, then restrict to events the VCC determined were
+## in fact Torsades. Include study phase.
+torsades_events <- ae_raw %>%
+  dplyr::select(
+    id, enroll_time, randomization_time,
+    matches("^ae\\_date\\_[0-9]$"),
+    # starts_with("ae_portion"),
+    starts_with("ae_vcc_final")
+  ) %>%
+  gather(key = ae_var_time, value = ae_value, ae_date_1:ae_vcc_final_3) %>%
+  separate(ae_var_time, into = c("ae_var", "ae_num"), sep = "\\_(?=[0-9]$)") %>%
+  spread(key = ae_var, value = ae_value) %>%
+  filter(ae_vcc_final == 3) %>%
+  mutate(
+    ## Determine days after consent, randomization to merge with daily data
+    ae_date = as.Date(ae_date, origin = "1970-1-1"),
+    days_since_consent = days_diff(ae_date, date(enroll_time)),
+    days_since_randomization = days_diff(ae_date, date(randomization_time)),
+    ## Indicator for when we merge with daily data
+    ae_torsades = TRUE
+  )
+  
+## Daily data: Based on arrhythmia, ECG, study drug, AE info, create a single
+## indicator for whether patient had Torsades on a given day
